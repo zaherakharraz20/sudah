9a6a3a0374bec67d3e94f0a4a27ecb6f70948288
zaherakharraz20 <s12323573@stu.najah.edu>
2025-12-09 15:52:03 +0200

    new js

---
+++ project.js
@@ -0,0 +1,8 @@
+const head = document.getElementById("h1")
+    const button = document.getElementById("button changed")
+    button.addEventListener(
+        "click" , () =>{
+            head.textContent=" welcome our page"
+
+        }
+    )
